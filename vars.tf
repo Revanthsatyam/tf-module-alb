7ed86e84c44845b8eb29f07e965cdfdb0d46e5af
Revanthsatyam <satyamrevanth@gmail.com>
2024-04-27 09:20:49 +0530

    Terraform

--- vars.tf
+++
@@ -1,8 +0,0 @@
-variable "internal" {}
-variable "lb_type" {}
-variable "tags" {}
-variable "env" {}
-variable "sg_port" {}
-variable "sg_ingress_cidr" {}
-variable "vpc_id" {}
-variable "subnets" {}
