caa15223787693886ef5c53ea0e558a90fdeb419
Revanthsatyam <satyamrevanth@gmail.com>
2024-04-27 12:27:36 +0530

    Terraform

--- locals.tf
+++ locals.tf
@@ -1,5 +1,6 @@
 locals {
 
-  tags = merge(var.tags, { tf-module-name = "alb" }, { env = var.env })
-
+  tags    = merge(var.tags, { tf-module-name = "alb" }, { env = var.env })
+  lb_name = var.internal ? "${var.env}-private-alb" : "${var.env}-public-alb"
+  sg_name = var.internal ? "${var.env}-private-alb-sg" : "${var.env}-public-alb-sg"
 }
